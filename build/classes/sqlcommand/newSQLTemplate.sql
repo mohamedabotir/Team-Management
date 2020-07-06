0802f686e9ff540acd95ef3a4236b79090f45d14
mohamedabotir <mohamedabotyear@gmail.com>
2020-07-06 21:47:17 +0200

    ManageTeam

---
+++ build/classes/sqlcommand/newSQLTemplate.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Threats
+ * Created: Jul 4, 2020
+ */
+
+create table user (id int(4) unsigned auto_increment primary key,name varchar(30) not null, password varchar(20) not null,index(id);
